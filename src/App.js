6cd66bf653f6a41244fa708019b43bef3fe022b8
Ashby Hernandez <asashby@hotmail.com>
2021-06-17 16:49:18 -0400

    Mi cambio

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hola mundo
+          hola mundo hello world
         </p>
         <a
           className="App-link"
