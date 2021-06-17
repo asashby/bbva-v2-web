d792fdbd1b11ac10f3fec05ecfa99f1ce7e44437
Rosangelica Feliz <rosangelica25@outlook.com>
2021-06-17 16:38:56 -0400

    Cambio de texto

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hola
+          hola mundo
         </p>
         <a
           className="App-link"
